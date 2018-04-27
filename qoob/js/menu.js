51894edf165f02bb356d85fc71e5b89d3cb27c97
MishaTheBest <32506187+MishaTheBest@users.noreply.github.com>
2018-04-27 14:01:47 +0300

    Add files via upload

--- qoob/js/menu.js
+++ qoob/js/menu.js
@@ -1,5 +1,7 @@
 var humSpans = document.querySelectorAll("#main-hum span");
 var mainMenuBtn = document.getElementById("main-hum");
+var menuBg = document.getElementById("menu-bg");
+var rightMneu = document.getElementById("rightside-menu");
 var visiable = false;
 mainMenuBtn.addEventListener("click",vis);
 function vis(){
@@ -13,12 +15,16 @@ function vis(){
         humSpans[2].style.top="8px";
         humSpans[2].style.transform="rotate(-45deg)";
         humSpans[0].style.opacity = "0.0";
+        menuBg.classList.add("colored-menu-bg");
+        rightMneu.classList.remove("hidden-menu");
     }else{
         visiable = false;
         humSpans[1].style.transform="rotate(0deg)";
         humSpans[1].style.width = "24px";
         humSpans[2].style.top="16px";
         humSpans[2].style.transform="rotate(0deg)";
-        humSpans[0].style.opacity = "1.0"
+        humSpans[0].style.opacity = "1.0";
+        menuBg.classList.remove("colored-menu-bg");
+        rightMneu.classList.add("hidden-menu");
     }
 }
