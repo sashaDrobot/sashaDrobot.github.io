bf48b46bded60c15af66fc0e005359480b049d19
sashaDrobot <32506175+sashaDrobot@users.noreply.github.com>
2018-09-03 22:49:37 +0300

    Add files via upload

--- admin/app.js
+++ admin/app.js
@@ -2,3 +2,23 @@ $('#toggler').on('click', function () {
     $('.wrapper').toggleClass('open-sidebar');
     $(this).toggleClass('collapsed');
 });
+
+const Dashboard = Vue.component('dashboard', {
+    template: '#dashboard',
+});
+
+const ViewUser = Vue.component('view-profile', {
+    template: '#view-profile',
+});
+
+let router = new VueRouter({
+    routes: [
+        { path: '/', component: Dashboard },
+        { path: '/view-profile', component: ViewUser },
+    ]
+});
+
+let vm = new Vue({
+    el: '#app',
+    router: router,
+});
